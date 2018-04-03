8f6d051922b6862f72115ca3ab3d95c6c07f04b3
velolisper <5976448+velolisper@users.noreply.github.com>
2018-04-03 16:50:34 -0400

    Update setup.js

--- moonmayor/setup.js
+++ moonmayor/setup.js
@@ -1,1 +1,18 @@
 // hello world
+// Matthew T Meneghini 2018
+
+var moongen = {
+  // moon generator functions
+
+}
+
+var io = {
+  // show the moon generated
+
+}
+
+var fs = {
+  // save the moon
+  
+}
+
