ce35ab2c73dafcfaf75721df29842360a383caa2
velolisper <5976448+velolisper@users.noreply.github.com>
2018-04-05 16:52:39 -0400

    Update blding.js

--- moonmayor/blding.js
+++ moonmayor/blding.js
@@ -1,1 +1,19 @@
 // for the building of new modules in the city.
+
+// Building Data Section
+const buildingList = []
+const infrastructureList = []
+
+class Building {
+ constructor(cost, needs, footprint) {
+  this.cost = cost;
+  this.needs = needs;
+  this.footprint = footprint;} 
+}
+
+// Building Code Section
+
+function newbldg() {
+
+
+}
